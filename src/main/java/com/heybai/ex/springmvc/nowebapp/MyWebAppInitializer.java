0ba3649b6cfe52b63f878e01ae90d06e295f7ebd
heybai <heybai@yandex-team.ru>
2014-06-05 15:47:16 +0300

    application without web.xml

---
+++ src/main/java/com/heybai/ex/springmvc/nowebapp/MyWebAppInitializer.java
@@ -0,0 +1,23 @@
+package com.heybai.ex.springmvc.nowebapp;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("classpath:spring/app-ctx.xml");
+
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+}
